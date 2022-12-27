39203adae7adb71545465a9adfcf46a4419261ae
ngiad <follownghia001@gmail.com>
2022-12-27 14:47:29 +0700

    day-1

--- src/Components/FoodterLayoutProduct/index.jsx
+++ src/Components/FoodterLayoutProduct/index.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import nhieuhangtot from "../../Images/nhieuhangtot.png"
-import sieutienloi from "../../Images/sieutienloi.png"
-import tiepkiem from "../../Images/tiepkiem.png"
-import yentam from "../../Images/yentam.png"
+import React from "react";
+import nhieuhangtot from "../../Images/nhieuhangtot.png";
+import sieutienloi from "../../Images/sieutienloi.png";
+import tiepkiem from "../../Images/tiepkiem.png";
+import yentam from "../../Images/yentam.png";
+import bocongthuong from "../../Images/bocongthuong.png";
+import dadangki from "../../Images/dadangki.png";
 
 const FooterLayoutProduct = () => {
+  const hotList =
+    "Đồng HồSHOPPING MallThế Giới Di ĐộngBánh Trung Thu Kinh ĐôBlack FridayTiviMỹ phẩm hàn quốcKem chống nắngChợ TốtShopeeCorona VirusĐồ chơi nấu ănLaptopiPhone 6Bàn phím cơQuạt"
+      .split(" ")
+      .map((item, index) => {
+        return (
+          <span className="hotSeo" key={index}>
+            {item}
+          </span>
+        );
+      });
   return (
-    <div className='FooterLayoutProduct'>
+    <div className="FooterLayoutProduct">
       <div className="top">
         <div className="content">
           <img src={nhieuhangtot} alt="Isnhieuhangtot" />
@@ -71,10 +83,51 @@ const FooterLayoutProduct = () => {
           </ul>
         </div>
       </div>
-      <div className="bottom"></div>
-      <div className="end"></div>
+      <div className="bottom">
+        <div className="contener">
+          <div className="left">
+            <p>Công ty Cổ phần Công nghệ Sen Đỏ, thành viên của Tập đoàn FPT</p>
+            <ul>
+              <li>
+                Số ĐKKD: 0312776486 - Ngày cấp: 13/05/2014, được sửa đổi lần thứ
+                20, ngày 26/04/2022.
+              </li>
+              <li>Cơ quan cấp: Sở Kế hoạch và Đầu tư TPHCM.</li>
+              <li>
+                Địa chỉ: Tầng 5, Tòa nhà A, Vườn Ươm Doanh Nghiệp, Lô D.01,
+                Đường Tân Thuận, Khu chế xuất Tân Thuận, Phường Tân Thuận Đông,
+                Quận 7, Thành phố Hồ Chí Minh, Việt Nam.
+              </li>
+              <li>Email: lienhe@sendo.vn</li>
+              <li>
+                <img src={dadangki} alt="dadangki" className="bo-cong-thuong" />{" "}
+                <img
+                  src={bocongthuong}
+                  alt="bocongthuong"
+                  className="bo-cong-thuong"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="right">
+            <form>
+              <label htmlFor="">
+                Đăng ký nhận bản tin ưu đãi khủng từ Sendo
+              </label>
+              <div>
+                <input type="text" placeholder="Email của bạn là " />
+                <button>Đăng kí</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="end">
+        <p>Top tìm kiếm</p>
+        {hotList}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterLayoutProduct
+export default FooterLayoutProduct;
